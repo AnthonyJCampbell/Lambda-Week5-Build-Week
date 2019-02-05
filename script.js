7b34c1841c84c65879d7ad4dd5ef0b146428cdb6
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-02-05 14:18:24 +0100

    Added desktop functionality to navbar

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "350px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     document.querySelector('#open').style.display = 'none';
     document.querySelector('#close').style.display = 'block';
     
@@ -10,8 +9,25 @@ function openNav() {
   /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.body.style.backgroundColor = "initial";
     document.querySelector('#open').style.display = 'block';
     document.querySelector('#close').style.display = 'none';
   }
+
+  window.addEventListener('resize', (() => {
+    // CASE: WINDOW IS LESS THAN 1024PX, WHICH MEANS THE OPEN-BUTTON OUGHT TO BE VISIBLE
+    if (window.innerWidth < 1024) {
+        document.querySelector('#open').style.display = 'block';
+    }  
+    // GENERAL CASE: MENU CLOSED AND WINDOW RESIZED
+    if (window.innerWidth >= 1024) {
+        document.querySelector('#open').style.display = 'none';
+        document.querySelector('#close').style.display = 'none';
+      }
+    // EDGE CASE: MENU IS OPEN, BUT WINDOW IS RESIZED
+    if (document.querySelector("#mySidenav").style.width > "1px" && window.innerWidth >= 1024) {
+        document.querySelector("#mySidenav").style.width = "0px";
+        document.querySelector('#close').style.display = 'none';
+        document.querySelector('#open').style.display = 'none';
+    }
+  }))
   
