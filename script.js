1421becfb86a0b992ef1129319a198d0d470740e
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-02-05 11:19:17 +0100

    Ordered LESS files and fixed side-menu behaviour

--- script.js
+++ script.js
@@ -2,11 +2,16 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "350px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.querySelector('#open').style.display = 'none';
+    document.querySelector('#close').style.display = 'block';
+    
   }
   
   /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "initial";
+    document.querySelector('#open').style.display = 'block';
+    document.querySelector('#close').style.display = 'none';
   }
   
