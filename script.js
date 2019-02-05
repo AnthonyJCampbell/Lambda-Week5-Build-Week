ce442f3717b7b5600b357456f4916b8c3c1942fd
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-02-05 10:33:35 +0100

    Defined breakpoints for tablet (768px) and desktop (1024px)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "350px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
