aa8749529bff493a35cdec2e2251f6471bdd6fdd
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-02-05 10:22:43 +0100

    Fixed side-menu, notwithstanding a gray-filtered effect on <body>

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const filterEl = document.getElementsByClassName('filter');
-filterEl.addEventListener('click', (() => console.log('I clicked the filtered element')));
-
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "initial";
+  }
+  
